cf223e6692dfe048a1f8eaaa37dc1f914f9cf4d8
srikanth <bonamsrikanthcan@gmail.com>
2019-10-21 22:32:58 -0400

    Initial API Call

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-import str from './models/Search'
+import axios from 'axios';
+
+async function getResult(query){
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const YOUR_APP_ID = '1e169500'
+    const key = '244bac5627ac84acc1bdb2b1509a479d';
+    const res = await axios(`${proxy}https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${key}`);
+    console.log(res);
+}
+
+getResult('bread');
