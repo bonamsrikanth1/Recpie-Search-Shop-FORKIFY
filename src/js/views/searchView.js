d6683010af9adba6d4e1a12d1ea8bf780dc05a0a
srikanth <bonamsrikanthcan@gmail.com>
2019-10-25 00:01:55 -0400

    limit ResPerPage

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,7 +12,7 @@ export const clearResults = () => {
 };
 
 //to cutshort the title of recipe if they are too lengthy and place... at the end
-// pasta with tomato and spinach
+// eg: pasta with tomato and spinach
 const limitRecipeTitle = (title, limit = 17) =>{
     const newTitle = [];
     if (title.length>limit){
@@ -30,8 +30,8 @@ const limitRecipeTitle = (title, limit = 17) =>{
 // to return all of our recepies 
 const renderRecipe = recipe =>{
     //here is where actual rendering happen
-    /*we need to display each recipe in the left side and for this in ES5 we inserted an html
-      in one line. where as in ES6 instead of tha we can simply write an inside an template string
+    /*we need to display each recipe in the left hand side and for this in ES5 we inserted an html
+      in one line. where as in ES6 instead we can simply copy html inside an template string
       as shown below  
     */
    const getID = (uri) => uri.split('#')[1];
@@ -51,6 +51,30 @@ const renderRecipe = recipe =>{
     elements.searchResList.insertAdjacentHTML('beforeend',markup);
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+const renderButtons = (page, numResults, resPerPage) => {
+    //1. we need to display next button on first page and page1 & page 2 navigation on 2nd page
+    // and prev page on the last page
+    // For this we need know the total number of pages present => no.of results/resPerPage ;
+    const pages = Math.ceil(numResults / resPerPage);
+    if(page === 1){
+        //only button to next page
+    }else if(page < pages){
+        // buttons to prev and next pages
+    }
+    else if(page === pages){
+        // only button to prev page
+    }
+        
+
 };
+
+
+
+/**Creating pages with 10 recipes per each
+   used slice method to cut down the list with start and end as shown */
+export const renderResults = (recipes,page = 1, resPerPage = 10) => {
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+    recipes.slice(start,end).forEach(renderRecipe);
+};
+
